2d86399489a472a50269723411d794572a0dfcdb
MrsKing2023 <smarshall84@yahoo.com>
2023-08-07 23:09:57 -0400

    created Banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Table from "./components/Banner";
+
 
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
